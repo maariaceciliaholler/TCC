c40b91f92a24901b5ab503a12a7246b82b95814b
kananak <kauanak21@gmail.com>
2020-11-26 22:21:34 -0300

    TASK00056 Eu não sei programar

---
+++ Mudanca 2/Jogo/letras.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php
+
+$imagem = array(); // Criamos um array com o nome das imagens.
+$imagem[1] = "../images/FasePalavras/ConfiguracaoA.png"; // Recheamos este array
+$imagem[2] = "../images/FasePalavras/ConfiguracaoB.png";
+$imagem[3] = "../images/FasePalavras/ConfiguracaoC.png";
+$imagem[4] = "../images/FasePalavras/ConfiguracaoD.png";
+$imagem[5] = "../images/FasePalavras/ConfiguracaoE.png";
+$imagem[6] = "../images/FasePalavras/ConfiguracaoF.png";
+$contador = count($imagem); // Criamos uma variavel para contar (count();) os dados que estão dentro do array.
+$aleatorio = rand(1,$contador); // Esta variável irá gerar um número aleatório (rand();), partindo do 1 até o número de dados que estão dentro do array..
+
+$resposta=isset($_POST['resposta'])?$_POST['resposta']: "";
+$cont=1;
+?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+	<link href="css/magnific-popup.css" rel="stylesheet">
+	<link href="../css/styles.css" rel="stylesheet">
+    <!-- Favicon  -->
+    <link rel="icon" href="../images/Detalhes/GloveLogo.png">
+    <style>
+       img{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+        #headerPopup{
+  width:75%;
+  margin:0 auto;
+}
+
+#headerPopup iframe{
+  width:100%;
+  margin:0 auto;
+}
+.coluna-50 {
+    width: 50%;
+}
+    </style>
+   
+    <title>SMARTGLOVE - Jogo</title>
+
+</head>
+<body class="header">
+    <nav class="navbar navbar bg" >
+        <a class="navbar-brand" href="../index.html" >
+            <img src="../images/LogoSite.png" class="d-inline-block align-top" alt="">
+        </a>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="<?php echo $imagem[$aleatorio]; ?>"/>
+        </div>
+      <div class="col">
+        <div class="card" style="width: 18rem; color: black;">
+          <div class="card-body">
+            <h5 class="card-title">Responda</h5>
+            <p class="card-text">Qual letra está representada por essa configuração de mão?</p>
+            <h5><?php echo $cont;?></h5>
+            <h5><?php echo $resposta;?></h5>
+            <form method="post">
+              <input type="text" id="resposta" name="resposta" value="<?= (!empty($_POST['resposta']))? 
+   $_POST['resposta'] : '' ?>">
+              <input type="submit" value="enviar">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php
+
+    // if($resposta == "a"){
+    //   if($imagem[$aleatorio] == $imagem[1]){
+    //     $cont= $cont+1;
+    //   }
+    // }
+    if($imagem[$aleatorio] == $imagem[1]){
+      echo "LETRA A";
+      $cont++;
+      if($resposta == "a" or $resposta == "A"){
+        echo "foi também";
+        
+      }
+    }
+    // if($imagem[$aleatorio] == $imagem[1] and $resposta == "a" or  $resposta == "A" and $imagem[$aleatorio] == $imagem[1]){
+    //   echo "Parabéns!";
+    //   $cont= $cont+1;
+    // }
+
+    // if($imagem[$aleatorio] == $imagem[2] and $resposta == "b"  or $imagem[$aleatorio] == $imagem[2] and $resposta == "B"){
+    //   echo "Parabéns!";
+    //   $cont= $cont+1;
+    // }
+    // if($imagem[$aleatorio] == $imagem[3] and $resposta == "c" or $imagem[$aleatorio] == $imagem[3] and $resposta == "C"){
+    //   echo "Parabéns!";
+    //   $cont= $cont+1;
+    // }
+    // if($imagem[$aleatorio] == $imagem[4] and $resposta == "d" or $imagem[$aleatorio] == $imagem[4] and $resposta == "D"){
+    //   echo "Parabéns!";
+    //    $cont= $cont+1;
+    // }
+    // if($imagem[$aleatorio] == $imagem[5] and $resposta == "e" or $imagem[$aleatorio] == $imagem[5] and $resposta == "E"){
+    //   echo "Parabéns!";
+    //    $cont= $cont+1;
+    // }
+    // if($imagem[$aleatorio] == $imagem[6] and $resposta == "f" or $imagem[$aleatorio] == $imagem[6] and $resposta == "F"){
+    //   echo "Parabéns!";
+    //   $cont++;
+    // }
+    if($cont == 5){
+      header('Location:homeJogo.html');
+    }
+?>
+</body>
+</html>
