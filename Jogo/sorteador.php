94f79ac9e532851979256829369f4014ce2e5f96
kananak <kauanak21@gmail.com>
2021-02-02 17:54:37 -0300

    TASK00088 não vaaaaaaaaaaaaaai

--- Jogo/sorteador.php
+++ Jogo/sorteador.php
@@ -2,23 +2,21 @@
 include_once "../conf/default.inc.php";
 require_once "../conf/Conexao.php";
 
-                $ext = strtolower(substr($_FILES['imagem']['name'],-4)); 
+               $ext = strtolower(substr($_FILES['imagem']['name'],-4)); 
                 $new_name = date("Y.m.d-H.i.s") . $ext; 
                 $dir = '../images/'.$_POST['submit']."/"; 
                 move_uploaded_file($_FILES['imagem']['tmp_name'], $dir.$new_name);
 
-
-                
+                $imagem = $_FILES['imagem']$new_name;
+                $valor = $_POST['valor'];
 
                 $pdo = Conexao::getInstance();
-                $stmt = $pdo->prepare("INSERT INTO ".$_POST['submit']."(imagem,valor) VALUES(:imagem,:valor)");
-                $stmt->bindParam(":imagem", $imagem, PDO::PARAM_STR); 
-                $stmt->bindParam(":valor", $valor, PDO::PARAM_STR); 
+                $stmt = $pdo->prepare("INSERT INTO ".$_POST['submit']."(imagem,valor) VALUES('".$imagem."', '".$valor."' )");
                 $stmt->execute();
                 var_dump($imagem);
 
 
-
+ 
 // testes
 // $dados= "";
 // $linha= "";
