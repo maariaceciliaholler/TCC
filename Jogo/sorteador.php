3d2f25ecc8b3233b0b5aaa12da9a53f64063778a
Mateus V <49762931+CreatorMA@users.noreply.github.com>
2021-02-02 16:57:13 -0300

    TASK00086 - imagem no bd

--- Jogo/sorteador.php
+++ Jogo/sorteador.php
@@ -2,19 +2,32 @@
 include_once "../conf/default.inc.php";
 require_once "../conf/Conexao.php";
 
+      
+
+                $ext = strtolower(substr($_FILES['imagem']['name'],-4)); 
+                $new_name = date("Y.m.d-H.i.s") . $ext; 
+                $dir = 'Upload/'.$_POST['submit']."/"; 
+                move_uploaded_file($_FILES['imagem']['tmp_name'], $dir.$new_name);
+
+
+
+
+
+
+
 
 // testes
-$dados= "";
-$linha= "";
+// $dados= "";
+// $linha= "";
 
-$pdo = Conexao::getInstance();
-$consulta = $pdo->query("SELECT * FROM fotosPalavras");
-$dados = array();
-while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-   echo $dados['nomePalavras'] = $linha['nomePalavras']."< ---- >";
-   echo $dados['valorPalavras']= $linha['valorPalavras']."<br>";
 
-}
+// $consulta = $pdo->query("SELECT * FROM fotosPalavras");
+// $dados = array();
+// while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+//    echo $dados['nomePalavras'] = $linha['nomePalavras']."< ---- >";
+//    echo $dados['valorPalavras']= $linha['valorPalavras']."<br>";
+
+// }
 //acabou
 
 ?>
