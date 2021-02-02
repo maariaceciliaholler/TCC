43dd1709310b67ed7699423f9d3e1b59d63f683f
Mary <52547463+maariaceciliaholler@users.noreply.github.com>
2021-02-02 15:51:12 -0300

    TASK00082 Forms para Add imagens

---
+++ Jogo/FormsAddImg.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+		<form id="form" action="" method="post">
+			<center>
+				<fieldset class="colorir">
+					<br><br>
+					<label>Download Imagem</label>
+					<input type="file" name="nome"><br><br>
+
+				    <label>Valor</label>
+				    <input type="text" name="valor"><br><br>
+
+					<input type="submit" value="Enviar">
+				
+				</fieldset>
+			</center>
+		</form>	
+</body>
+</html>
