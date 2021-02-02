326204f359e626c0e67c7083f8a795eb866d63ab
kananak <kauanak21@gmail.com>
2021-02-02 16:24:15 -0300

    TASK00085 tirei

--- Jogo/sorteador.php
+++ Jogo/sorteador.php
@@ -2,36 +2,6 @@
 include_once "../conf/default.inc.php";
 require_once "../conf/Conexao.php";
 
-if(isset($_FILES['nomePalavras']))
-   {
-      require 'FormsAddImg.php';
-
-      date_default_timezone_set("Brazil/East");
-
-      $nomePalavras     = $_FILES['nomePalavras'];
-      $tmp_name = $_FILES['nomePalavras'];
-
-      $allowedExts = array(".gif", ".jpeg", ".jpg", ".png", ".bmp");
-
-      $dir = 'uploads/';
-
-      for($i = 0; $i < count($tmp_name); $i++)
-      {
-         $ext = strtolower(substr($nomePalavras[$i],-4));
-
-         if(in_array($ext, $allowedExts))
-         {
-            $new_name = date("Y.m.d-H.i.s") ."-". $i . $ext;
-
-            $image = WideImage::load($tmp_name[$i]);
-
-            $image = $image->resize(170, 180, 'outside');
-            $image = $image->crop('center', 'center', 170, 180);
-
-            $image->saveToFile($dir.$new_name);
-         }
-      }
-   }
 
 // testes
 $dados= "";
@@ -45,5 +15,6 @@ while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
    echo $dados['valorPalavras']= $linha['valorPalavras']."<br>";
 
 }
+//acabou
 
 ?>
