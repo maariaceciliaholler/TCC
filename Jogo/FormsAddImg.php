9577a54f9428df2d4cc913df1b2c55c86fd9717e
Mary <52547463+maariaceciliaholler@users.noreply.github.com>
2021-02-02 15:53:26 -0300

    TASK00083 add

--- Jogo/FormsAddImg.php
+++ Jogo/FormsAddImg.php
@@ -5,13 +5,27 @@
 <body>
 		<form id="form" action="" method="post">
 			<center>
+				<h3>Fotos Palavras</h3>
 				<fieldset class="colorir">
 					<br><br>
 					<label>Download Imagem</label>
-					<input type="file" name="nome"><br><br>
+					<input type="file" name="nomePalavras"><br><br>
 
 				    <label>Valor</label>
-				    <input type="text" name="valor"><br><br>
+				    <input type="text" name="valorPalavras"><br><br>
+
+					<input type="submit" value="Enviar">
+				
+				</fieldset>
+
+				<h3>Fotos Letras</h3>
+				<fieldset class="colorir">
+					<br><br>
+					<label>Download Imagem</label>
+					<input type="file" name="nomeLetras"><br><br>
+
+				    <label>Valor</label>
+				    <input type="text" name="valorLetras"><br><br>
 
 					<input type="submit" value="Enviar">
 				
